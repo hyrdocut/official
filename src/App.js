5a90526ca569ff86b7fba85f6723671900f6aa4d
hrithikrtiwari <hrithikrtiwari@gmail.com>
2021-04-25 19:43:33 +0530

    .

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Welcome to Hydrocut, the website development is in progress<br/>
+         
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h6>Launching on 26<sup>th</sup> April</h6>
+        
       </header>
     </div>
   );
